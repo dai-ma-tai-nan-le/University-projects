693e5d8eb311b75f658f1758595c8c7b04b44cbc
一壶清酒对风饮，两根香烟与风吹 <118416770+dai-ma-tai-nan-le@users.noreply.github.com>
2023-07-11 14:27:09 +0800

    Add files via upload

---
+++ 毕设/最终答辩/毕设最终提交材料/JK19851002娄建鹏/JK19851002娄建鹏工程及安装说明/project/xszlglxthd/src/test/java/com/ljp/demo/DemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.ljp.demo;
+
+import com.ljp.demo.dao.UserDao;
+import com.ljp.demo.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+//        String password="123456";
+//        System.out.println(new BCryptPasswordEncoder().encode(password).toString());
+
+//        System.out.println(new Date());
+    }
+
+}
